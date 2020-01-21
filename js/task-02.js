214b0954852458918eddb9d6e9cf5027d0dec4dd
Victor Batigin <bixs85@gmail.com>
2020-01-21 18:24:34 +0200

    fix task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
-const total = 100;
-const ordered = 80;
+// const total = 100;
+// const ordered = 80;
 
-if (total < ordered) {
-  console.log(`На складе не достаточно товаров!`);
-} else {
-  console.log(`Заказ оформлен, с Вами свяжется менеджер`);
-}
+// if (total < ordered) {
+//   console.log(`На складе не достаточно товаров!`);
+// } else {
+//   console.log(`Заказ оформлен, с Вами свяжется менеджер`);
+// }
+
+const total = 100;
+const numberInputRef = document.querySelector('input[name="task-2"]');
+const btnRef = document.querySelector("button");
+
+btnRef.addEventListener("click", function() {
+  console.log(numberInputRef.value);
+  const ordered = Number(numberInputRef.value);
+
+  if (total < ordered) {
+    alert(`На складе не достаточно товаров!`);
+  } else {
+    alert(`Заказ оформлен, с Вами свяжется менеджер`);
+  }
+});
